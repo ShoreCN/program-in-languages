3a6783c657e523737cf3e52379d5f688760ccc23
Shr <c.shore.cn@gmail.com>
2024-03-29 03:13:49 +0800

    test performance with array index

--- leetcode122/stock_trading_time2.py
+++ leetcode122/stock_trading_time2.py
@@ -49,6 +49,14 @@ class Solution:
         
         return max_profit
     
+    def maxProfit2(self, prices: List[int]) -> int:
+        max_profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                max_profit += prices[i] - prices[i-1]
+        
+        return max_profit
+    
 if __name__ == '__main__':
     prices = [7,1,5,3,6,4]
     solution = Solution()
